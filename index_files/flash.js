15d1c985592a34dcffa91b525bdd3e0710970081
richard.xue <richard.xue@fairfaxmedia.co.nz>
2021-04-02 10:45:44 +1300

    init

---
+++ index_files/flash.js
@@ -0,0 +1,43 @@
+//jquery特效开始
+$(document).ready(function(){
+
+//花旗参的六大好处
+$(".home_item_img img").hover(function(){
+	var new_img=$(this).attr("data-new");
+	if(new_img!=""){
+		$(this).attr("src",new_img);
+	}
+},function(){
+	var old_img=$(this).attr("data-old");
+	if(old_img!=""){
+		$(this).attr("src",old_img);	
+	}
+});
+
+
+//全屏轮换
+TouchSlide({ 
+	slideCell:"#slideBox",
+	titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+	mainCell:".bd ul", 
+	effect:"leftLoop", 
+	interTime:5000,
+	autoPlay:true,//自动播放
+	autoPage:true //自动分页
+});
+
+//中部轮换
+TouchSlide({ 
+	slideCell:"#adBox",
+	titCell:".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+	mainCell:".bd ul", 
+	effect:"leftLoop", 
+	interTime:5000,
+	autoPlay:true,//自动播放
+	autoPage:true //自动分页
+});
+
+
+						
+});
+//jquery 结束
